c4e18b0d0db812d5514908ea173c5c51153c599d
Yann <116547223+yannletouzey@users.noreply.github.com>
2023-12-14 15:45:41 +0100

    add responsive header/menu

--- src/components/Burger.jsx
+++ src/components/Burger.jsx
@@ -1,15 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
-const Burger = () => {
-    const [stateHeaderContainer, setStateHeaderContainer] = useState('is-closed');
+const Burger = ({stateContactAppear, setStateContactAppear, menuRef}) => {
+    const [stateBurgerContainer, setStateBurgerContainer] = useState('is-closed');
     const [stateBurger, setStateBurger] = useState('is-closed');
-    const handleClick = () => {
-        stateHeaderContainer === 'is-closed' ? setStateHeaderContainer('is-opened') : setStateHeaderContainer('is-closed');
-        stateBurger === 'is-closed' ? setStateBurger('is-opened') : setStateBurger('is-closed');
+    const [resize, setResize] = useState(window.innerWidth);
+
+    const burgerRef = useRef();
+
+    const handleResize = () => {
+        setResize(window.innerWidth);
     }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        if (resize >= 800) {
+            setStateContactAppear('is-opened');
+            setStateBurger('is-closed');
+            setStateBurgerContainer('is-closed');
+        } else {
+            if (stateBurger === 'is-closed') {
+                setStateContactAppear('is-closed');
+                // menuRef.current.style.transform = `translateX(0)`;
+            }
+        }
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [handleResize]);
+
+    const handleClick = () => {
+        stateBurgerContainer === 'is-closed' ? setStateBurgerContainer('is-opened') : setStateBurgerContainer('is-closed');
+        stateBurger === 'is-closed' ? setStateBurger('is-opened') : setStateBurger('is-closed');
+        stateContactAppear === 'is-opened' ? setStateContactAppear('is-closed') : setStateContactAppear('is-opened');
+    }
+
+    useEffect(() => {
+        const offset = menuRef.current.clientWidth;
+        if (stateBurger === 'is-opened') {
+            // burgerRef.current.style.transform = `translateX(-${offset + 20}px)`;
+            // menuRef.current.style.transform = `translateX(-${offset}px)`;
+        } else {
+            // burgerRef.current.style.transform = `translateX(0)`;
+            // menuRef.current.style.transform = `translateX(0)`;
+        }
+    }, [stateBurger])
+
     return (
-        <div id="header__container" className={`header__container ${stateHeaderContainer}`} onClick={handleClick}>
-            <button id="header__container__burger" className={`header__container__burger ${stateBurger}`}>
+        <div
+            id="header__burger" 
+            className={`header__burger ${stateBurgerContainer}`} 
+            ref={burgerRef} 
+            onClick={handleClick}
+        >
+            <button 
+                id="header__burger__button" 
+                className={`header__burger__button ${stateBurger}`}
+            >
                 <span></span>
             </button>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,113 +1,40 @@
+import { useEffect, useRef, useState } from 'react';
 import linkedInLogo from '../assets/img/logos/linkedin.png';
 import githubLogo from '../assets/img/logos/github.png';
 import twitterLogo from '../assets/img/logos/logo-x.png';
 import Burger from './Burger';
+import CubeContact from './CubeContact';
 
 const Header = () => {
+    const [stateContactAppear, setStateContactAppear] = useState('is-closed');
+    const menuRef = useRef();
+
     return (
         <header className="header">
             <h2 id="header__title" className="header__title">
                 <span id="header__title--firstname" className="header__title--firstname">Yann</span>
                 <span id="header__title--lastname" className="header__title--lastname">Letouzey</span>
             </h2>
-            <Burger />
-            <menu id="header__menu" className="header__menu">
-                <div id="cube-one" className="header__menu--cube">
-                    <div className="face front">
-                        <a href="https://www.linkedin.com/in/yann-letouzey-84374524b/" target="_blank" rel="noopener noreferrer">
-                            <img src={linkedInLogo} alt="Logo LinkedIn"/>
-                        </a>
-                    </div>
-                    <div className="face back">
-                        <a href="https://www.linkedin.com/in/yann-letouzey-84374524b/" target="_blank" rel="noopener noreferrer">
-                            <img src={linkedInLogo} alt="Logo LinkedIn"/>
-                        </a>
-                    </div>
-                    <div className="face left">
-                        <a href="https://www.linkedin.com/in/yann-letouzey-84374524b/" target="_blank" rel="noopener noreferrer">
-                            <img src={linkedInLogo} alt="Logo LinkedIn"/>
-                        </a>
-                    </div>
-                    <div className="face right">
-                        <a href="https://www.linkedin.com/in/yann-letouzey-84374524b/" target="_blank" rel="noopener noreferrer">
-                            <img src={linkedInLogo} alt="Logo LinkedIn"/>
-                        </a>
-                    </div>
-                    <div className="face top">
-                        <a href="https://www.linkedin.com/in/yann-letouzey-84374524b/" target="_blank" rel="noopener noreferrer">
-                            <img src={linkedInLogo} alt="Logo LinkedIn"/>
-                        </a>
-                    </div>
-                    <div className="face bottom">
-                        <a href="https://www.linkedin.com/in/yann-letouzey-84374524b/" target="_blank" rel="noopener noreferrer">
-                            <img src={linkedInLogo} alt="Logo LinkedIn"/>
-                        </a>
-                    </div>
-                </div>
-                <div id="cube-two" className="header__menu--cube">
-                    <div className="face front">
-                        <a href="https://github.com/yannletouzey" target="_blank" rel="noopener noreferrer">
-                            <img src={githubLogo} alt="Logo GutHub"/>
-                        </a>
-                    </div>
-                    <div className="face back">
-                        <a href="https://github.com/yannletouzey" target="_blank" rel="noopener noreferrer">
-                            <img src={githubLogo} alt="Logo GutHub"/>
-                        </a>
-                    </div>
-                    <div className="face left">
-                        <a href="https://github.com/yannletouzey" target="_blank" rel="noopener noreferrer">
-                            <img src={githubLogo} alt="Logo GutHub"/>
-                        </a>
-                    </div>
-                    <div className="face right">
-                        <a href="https://github.com/yannletouzey" target="_blank" rel="noopener noreferrer">
-                            <img src={githubLogo} alt="Logo GutHub"/>
-                        </a>
-                    </div>
-                    <div className="face top">
-                        <a href="https://github.com/yannletouzey" target="_blank" rel="noopener noreferrer">
-                            <img src={githubLogo} alt="Logo GutHub"/>
-                        </a>
-                    </div>
-                    <div className="face bottom">
-                        <a href="https://github.com/yannletouzey" target="_blank" rel="noopener noreferrer">
-                            <img src={githubLogo} alt="Logo GutHub"/>
-                        </a>
-                    </div>
-                </div>
-                <div id="cube-three" className="header__menu--cube">
-                    <div className="face front">
-                        <a href="https://twitter.com/yzey940" target="_blank" rel="noopener noreferrer">
-                            <img src={twitterLogo} alt="Logo x"/>
-                        </a>
-                    </div>
-                    <div className="face back">
-                        <a href="https://twitter.com/yzey940" target="_blank" rel="noopener noreferrer">
-                            <img src={twitterLogo} alt="Logo x"/>
-                        </a>
-                    </div>
-                    <div className="face left">
-                        <a href="https://twitter.com/yzey940" target="_blank" rel="noopener noreferrer">
-                            <img src={twitterLogo} alt="Logo x"/>
-                        </a>
-                    </div>
-                    <div className="face right">
-                        <a href="https://twitter.com/yzey940" target="_blank" rel="noopener noreferrer">
-                            <img src={twitterLogo} alt="Logo x"/>
-                        </a>
-                    </div>
-                    <div className="face top">
-                        <a href="https://twitter.com/yzey940" target="_blank" rel="noopener noreferrer">
-                            <img src={twitterLogo} alt="Logo x"/>
-                        </a>
-                    </div>
-                    <div className="face bottom">
-                        <a href="https://twitter.com/yzey940" target="_blank" rel="noopener noreferrer">
-                            <img src={twitterLogo} alt="Logo x"/>
-                        </a>
-                    </div>
-                </div>
+            <Burger stateContactAppear={stateContactAppear} setStateContactAppear={setStateContactAppear} menuRef={menuRef} />
+            <menu id="header__contact" className={`header__contact ${stateContactAppear}`} ref={menuRef}>
+                <CubeContact 
+                    id="cube-one" 
+                    link="link" 
+                    logo={linkedInLogo} 
+                    alt="Logo LinkedIn"
+                />
+                <CubeContact 
+                    id="cube-two" 
+                    link="link" 
+                    logo={githubLogo} 
+                    alt="Logo GutHub"
+                />
+                <CubeContact 
+                    id="cube-three" 
+                    link="link" 
+                    logo={twitterLogo} 
+                    alt="Logo Twitter"
+                />
             </menu>
         </header>
     )
