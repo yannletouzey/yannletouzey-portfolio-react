b2250bb5f7dd5333e2004a9f2cab60749b0bb0b3
Yann <116547223+yannletouzey@users.noreply.github.com>
2023-12-14 17:42:54 +0100

    add dedscription

---
+++ src/components/Description.jsx
@@ -0,0 +1,8 @@
+const Description = () => {
+    return (
+        <section class="main__description">
+            <p>Développeur autodidacte spécialisé dans la création d'architectures back-end. Ma passion s'étend au-delà, avec un intérêt marqué pour l'intégration front-end parsemée de petites animations. Explorez mon portfolio pour découvrir une fusion réussie entre performances back-end et esthétique interactive dans des projets déjà réalisés.</p>
+        </section>
+    )
+}
+export default Description;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import Carousel from "./Carousel";
+import Description from "./Description";
 import NextButton from "./NextButton";
 import PrevButton from "./PrevButton";
 
@@ -8,6 +9,7 @@ const Main = () => {
             <div id="banner" className="banner">
                 <h1 id="banner__panel" className="banner__panel">Developpeur Web Developpeur Web Developpeur Web </h1>
             </div>
+            <Description />
             {/* <section id="container" className="container"> */}
                 {/* <div className="button">
                     <PrevButton />
