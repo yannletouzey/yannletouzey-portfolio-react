00bd6b7372324942ad3e632d24dac415a62acbc0
Yann <116547223+yannletouzey@users.noreply.github.com>
2023-12-21 19:41:19 +0100

    modification popup details

--- src/components/CarouselDesc.jsx
+++ src/components/CarouselDesc.jsx
@@ -4,11 +4,6 @@ import CarouselDescTechno from "./CarouselDescTechno.jsx";
 import CarouselDescLink from "./CarouselDescLink.jsx";
 
 const CarouselDesc = ({dataCarouselElement}) => {
-    const [hover, setHover] = useState(false);
-    const msgMore = useRef();
-    useEffect(() => {
-        // hover ? msgMore.current.style.display = "inline" : msgMore.current.style.display = "none";
-    }, [hover]);
     return (
         <div className="container__carousel--desc" key={`container__carousel--desc-${dataCarouselElement.id}`}>
             <CarouselDescLink 
@@ -20,8 +15,7 @@ const CarouselDesc = ({dataCarouselElement}) => {
             />
             {dataCarouselElement.descSmall && 
                 <>
-                    <Link to={dataCarouselElement.page} className="container__carousel--desc--more" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}><span ref={msgMore} className={`container__carousel--desc--more--msg`}>un peu plus de détails</span></Link>
-                    
+                    <Link to={dataCarouselElement.page} className="container__carousel--desc--more"><span className={`container__carousel--desc--more--msg`}>un peu plus de détails</span></Link>
                 </>
             }
         </div>
--- src/components/CarouselDescLink.jsx
+++ src/components/CarouselDescLink.jsx
@@ -1,32 +1,65 @@
 import React, { useEffect, useState, useRef } from "react";
 const CarouselDescLink = ({dataCarouselElement}) => {
+    const linkRef = useRef();
+    const [link, setLink] = useState();
     const [hover, setHover] = useState();
     const [inline, setInline] = useState();
     const tooltip = useRef();
     useEffect(() => {
         setInline(dataCarouselElement.inline);
-    }, []);
+    }, [inline]);
     useEffect(() => {
         hover ? tooltip.current.style.display = "inline" : tooltip.current.style.display = "none";
     }, [hover])
+    useEffect(() => {
+        // if (inline && link) {
+        //     const text = linkRef.current.children[0].children[0].textContent;
+        //     linkRef.current.children[0].children[0].innerHTML = '';
+
+        //     const text2 = linkRef.current.children[1].children[0].textContent;
+        //     linkRef.current.children[1].children[0].innerHTML = '';
+        //     const textArray = Array.from(text);
+        //     const textArray2 = Array.from(text2);
+        //     textArray.forEach((e, i) => {
+        //         const span = document.createElement('span');
+        //         span.style.display = "inline-block";
+        //         span.style.transition = "0.5s";
+        //         span.style.transitionDelay = `${0.03 * i}s`
+        //         span.id = i;
+        //         span.textContent = e;
+        //         linkRef.current.children[0].children[0].appendChild(span);
+        //     });
+        //     textArray2.forEach((e, i) => {
+        //         const span = document.createElement('span');
+        //         span.style.display = "inline-block";
+        //         span.style.transition = "0.5s";
+        //         span.style.transitionDelay = `${0.03 * i}s`
+        //         span.id = i;
+        //         span.textContent = e;
+        //         linkRef.current.children[1].children[0].appendChild(span);
+        //     });
+        // }
+    }, [link])
     return (
         <div className="container__carousel--desc--link">
             {inline ? 
-                <a href={dataCarouselElement.link} target="_blank" rel="noopener noreferrer">
-                    <div className="containerLink s1">
-                        {dataCarouselElement.title}
-                    </div>
-                </a> 
+                <div className="container__carousel--desc--link--inline" ref={linkRef} onMouseEnter={() => setLink(true)} onMouseLeave={() => setLink(false)}>
+                    <a href={dataCarouselElement.link} target="_blank" rel="noopener noreferrer">
+                        <div className="containerLink s1">
+                            {dataCarouselElement.title}
+                        </div>
+                    </a> 
+                    <a href={dataCarouselElement.link} target="_blank" rel="noopener noreferrer">
+                        <div className="containerLink s2">
+                            {dataCarouselElement.title}
+                        </div>
+                    </a>
+                </div>
                 : 
                 <>
                     <p className="container__carousel--msgNotOnline" ref={tooltip}>"{dataCarouselElement.title}" n'est pas encore en ligne</p>
                     <p onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>{dataCarouselElement.title}</p>
                 </>}
-            {/* <a href={dataCarouselElement.link} target="_blank" rel="noopener noreferrer">
-                <div className="containerLink s2">
-                    {dataCarouselElement.title}
-                </div>
-            </a> */}
         </div>
     )
 }
