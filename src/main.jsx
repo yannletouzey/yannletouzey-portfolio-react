72d163e15999a9397714abdf8830adbb800534ef
yannletouzey <y.letouzey@gmail.com>
2024-03-04 19:07:10 +0100

    refact router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import './scss/style.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    </BrowserRouter>
 )
