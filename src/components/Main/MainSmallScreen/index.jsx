c819789c354badb5b5275879da0979e696e697fa
yannletouzey <y.letouzey@gmail.com>
2024-05-02 23:46:25 +0200

    fix message error

--- src/components/Main/MainSmallScreen/index.jsx
+++ src/components/Main/MainSmallScreen/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function MainSmallScreen() {
   return (
-    <p className="main__text">Ce site n'est pas compatible avec cette appareil.</p>
+    <>
+      <p className="main__text">Ce site n'est pas compatible avec cette appareil<br/><br/>Incompatible Safari</p>
+    </>
   )
 }
