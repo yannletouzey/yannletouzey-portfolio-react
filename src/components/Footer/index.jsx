0c041268291f3c5c3ada669a8b63dd9c62a89fbc
yannletouzey <y.letouzey@gmail.com>
2024-05-03 15:49:02 +0200

    fix footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <a href="mailto:yzey940@gmail.com" className='footer__contact'>Contact email</a>
-      <Link to="/about" className='footer__contact'>About</Link>
+      <Link to="/about" className='footer__contact'>A propos</Link>
     </footer>
   )
 }
