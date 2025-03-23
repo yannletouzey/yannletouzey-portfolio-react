1c0e3c0394330214a14bfc0419ae67e26de7853d
yannletouzey <y.letouzey@gmail.com>
2025-03-23 15:15:54 +0100

    add link all projects

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="footer">
       <a href="mailto:yzey940@gmail.com" className='footer__contact'>Contact email</a>
       <Link to="/about" className='footer__contact'>A propos</Link>
-      <Link to="/projects" className='footer__project'>Autres projets</Link>
+      <a href="https://yannletouzey-all-projects.vercel.app/" className='footer__project'>Autres projets</a>
     </footer>
   )
 }
